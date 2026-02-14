81002fba7611b3219cebd154ec9fdb5a23984909
VVS <97415302+VVS-l@users.noreply.github.com>
2026-02-14 00:25:47 -0600

    Add files via upload

---
+++ iframe.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html><html><head>
+        <title>MV</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/uikit.min.css">
+        <script src="js/jquery.min_1.js"></script>
+        <script src="js/uikit.min_1.js"></script>
+        <script src="js/979d0b3523.js" crossorigin="anonymous"></script>
+        <style> 
+        video { 
+					position: absolute;
+					top: -25%;
+					left: -25%;
+					width: 150%;
+					height: 150%;
+
+				}
+
+				video {
+					position: absolute;
+					top: 0;
+					bottom: 0;
+					right: 0;
+					left: 0;
+					margin: auto;
+					min-height: 50%;
+					min-width: 50%;
+				}
+        </style>
+    </head>
+    <body>
+    <div class="video-container">
+			<video id="MVideo" autoplay="" loop="" muted="" playsinline="" uk-video="autoplay: inview;automute: true" onclick="this.paused? this.play() : this.pause()">
+				<source src="media/High_School.mp4" type="video/mp4">
+				<source src="media/High_School.webm" type="video/webm">
+			</video>
+		</div>
+    <script defer="" src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" data-cf-beacon="{" version":"2024.11.0","token":"b317793d45cd4f2799569505dc5f6b0a","server_timing":{"name":{"cfcachestatus":true,"cfedge":true,"cfextpri":true,"cfl4":true,"cforigin":true,"cfspeedbrain":true},"location_startswith":null}}"="" crossorigin="anonymous"></script>
+
+
+</body></html>
